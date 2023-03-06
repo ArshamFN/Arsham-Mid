9a14da3902c5cfc015dade0d3be05c4340e3c2e8
arshamfn <arshamfaghihnasiri@gmail.com>
2023-03-05 22:16:28 -0500

    first arshams commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  "URI":"mongodb+srv://Arjen:6D9lukYFyINH868e@mongodbserver.vuncwmt.mongodb.net/book_store?retryWrites=true&w=majority"
 };
